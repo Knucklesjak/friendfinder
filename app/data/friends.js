86ed4aa09c11092b90013b6f6a801c4558cf830e
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-27 16:26:08 -0800

    added photo links to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,44 @@
 var friends = [
 	{
 		name: "Willie Stokes",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		photo: "http://s.storage.akamai.coub.com/get/b91/p/coub/simple/cw_timeline_pic/07840c242f1/9a7587f0ee77e8f364995/big_1473901909_image.jpg",
+		sex: "Male",
 		scores: [5, 5, 4, 4, 4, 4, 5, 4, 5, 5]
 	},
 	{
 		name: "George Constanza",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://media.giphy.com/media/3o7TKqvzk3tqUrVpE4/giphy.gif",
+		sex: "Male",
 		scores: [3, 5, 3, 3, 3, 3, 3, 3, 3, 2]
 	},
 	{
 		name: "Daria Morgendoffer",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Daria_Morgendorffer.png/222px-Daria_Morgendorffer.png",
+		sex: "Female",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 1]
 	},
 	{
 		name: "Norman Bates",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		photo: "https://virily.com/wp-content/uploads/2018/01/perkins_200-369c5d65aec1a1c3ecf466c298b7dc9b40aa5c26-s6-c10-758x1095.jpg",
+		sex: "Male",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Lisa Loopner",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://www.nbc.com/sites/nbcunbc/files/files/images/2015/4/21/140207_2721497_The_Judy_Miller_Show_anvver_6.jpg",
+		sex: "Female",
 		scores: [2, 2, 2, 2, 4, 4, 3, 2, 1, 1]
 	},
 	{
 		name: "Moe Szyslak",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://thumbs.gfycat.com/IdealisticVelvetyKinkajou-max-1mb.gif",
+		sex: "Male",
 		scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
 	},
 	{
 		name: "Eunice Harper Higgins",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://imgix.ranker.com/user_node_img/49/960550/original/eunice-harper-higgins-tv-characters-photo-u1?w=280&h=280&fit=crop&crop=faces&q=50&fmt=jpg",
+		sex: "Female",
 		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	}
 ];
