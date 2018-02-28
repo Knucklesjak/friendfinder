50870331373f5738e06c2f1f6632d4f457a27052
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-27 16:05:22 -0800

    adding htmlRoutes, apiRoute, and server. Need to do CSS, clean code, and test

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+	{
+		name: "Willie Stokes",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		scores: [5, 5, 4, 4, 4, 4, 5, 4, 5, 5]
+	},
+	{
+		name: "George Constanza",
+		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		scores: [3, 5, 3, 3, 3, 3, 3, 3, 3, 2]
+	},
+	{
+		name: "Daria Morgendoffer",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 1]
+	},
+	{
+		name: "Norman Bates",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Lisa Loopner",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [2, 2, 2, 2, 4, 4, 3, 2, 1, 1]
+	},
+	{
+		name: "Moe Szyslak",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+	},
+	{
+		name: "Eunice Harper Higgins",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	}
+];
+
+module.exports = friends;
